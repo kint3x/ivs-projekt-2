2b378a3302e33dad2704ae16230c1b8ddd521dfe
xmatej55 <martin@matejka.xyz>
2020-03-23 18:28:47 +0100

    pridanie math kniznice a include do kodu

---
+++ src/math.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mathlib
+{
+	public class math
+	{
+		public static double Sucet(double a, double b)
+		{
+			return a + b;
+		}
+	}
+}
