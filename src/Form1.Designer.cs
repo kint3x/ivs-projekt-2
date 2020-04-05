020466b865920c5756d03dfee2fe64874a4481a3
xbenov00 <xbenov00@vutbr.cz>
2020-04-05 20:11:24 +0200

    test1

--- src/Form1.Designer.cs
+++ src/Form1.Designer.cs
@@ -444,4 +444,3 @@
         private System.Windows.Forms.TextBox display;
     }
 }
-
---
+++ src/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    class Test
+    {
+        [TestClass]
+        public class Test
+        {
+            [TestMethod]
+            public void TestAdd()
+            {
+                int vysledek = obj.Add(1, 1);
+                Assert.AreEqual(20, vysledek);
+            }
+
+        }
+    }
+}
