663522a4b5f095b90f0a2374238925138d24ca0b
xbenov00 <xbenov00@vutbr.cz>
2020-04-14 23:13:27 +0200

    TestOper

--- src/Test.cs
+++ src/Test.cs
@@ -23,6 +23,89 @@ namespace Testing
                 }
 
             }
-            // Test odčítanie
+        // Test odčítanie
+        public void TestSub(double a, double b)
+        {
+            a = 2;
+            b = 1;
+            if (a - b == 1)
+            {
+                Console.WriteLine("TestSub successful.");
+            }
+            else
+            {
+                Console.WriteLine("TestSub failed!");
+            }
+
+        }
+
+        public void TestMul(double a, double b)
+        {
+            a = 2;
+            b = 3;
+            if (a * b == 6)
+            {
+                Console.WriteLine("TestSub successful.");
+            }
+            else
+            {
+                Console.WriteLine("TestSub failed!");
+            }
+
+        }
+
+        public void TestDiv(double a, double b)
+        {
+            a = 0;
+            b = 1;
+            try
+            {
+                try
+                {
+                    // Throws ' cannot divide by zero ' error
+                    double error = b / a;
+                }
+                catch (DivideByZeroException)
+                {
+                    // Throws error again of course
+                    double somenum = b / a;
+                }
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("I can never make it here ...");
+            }
+
+        }
+
+        public void TestFact(double a)
+        {
+            a = 2;
+            if (a == 2)
+            {
+                Console.WriteLine("TestSub successful.");
+            }
+            else
+            {
+                Console.WriteLine("TestSub failed!");
+            }
+
+        }
+
+        public void TestPow(double a, double n)
+        {
+            a = 2;
+            n = 2;
+            if (a / n == 1)
+            {
+                Console.WriteLine("TestSub successful.");
+            }
+            else
+            {
+                Console.WriteLine("TestSub failed!");
+            }
+
+
+        }
     }
 }
