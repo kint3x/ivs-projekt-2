9eef559fba738d6a7e61518cd131213069fe8873
xdvors14 <dvorsky.hjk@gmail.com>
2020-04-14 17:25:27 +0200

    Sfunkčnenie mocniny a faktorialu, oprava tlacidiel "1" ," 7" a "ac", pridana funkcnost na tlacidlo "."

--- src/Form1.cs
+++ src/Form1.cs
@@ -18,6 +18,9 @@ namespace src
         private int delenie = 0;
         private int rozdiel = 0;
         private int enter = 0;
+        private int bodka = 0;
+        private int mocnina = 0;
+        private int faktorial = 0;
         
         private double a;
         private double b;
@@ -29,21 +32,40 @@ namespace src
 
         public void btn_1_Click(object sender, EventArgs e)
         {
-            if (display.Text == "0")
+            if (faktorial == 1)
             {
-                display.Clear();
+                display.Text = display.Text;
             }
-            
-            display.Text = display.Text + "1";
+            else
+            {
+                if (display.Text == "0" || enter == 1)
+                {
+                    display.Clear();
+                    enter = 0;
+                    bodka = 0;
+                }
+
+                display.Text = display.Text + "1";
+            }
         }
 
         public void btn_7_Click(object sender, EventArgs e)
         {
-            if (display.Text == "0")
+            if (faktorial == 1)
             {
-                display.Clear();
+                display.Text = display.Text;
             }
-            display.Text = display.Text + "7";
+            else
+            {
+                if (display.Text == "0" || enter == 1)
+                {
+                    display.Clear();
+                    enter = 0;
+                    bodka = 0;
+                }
+
+                display.Text = display.Text + "7";
+            }
         }
 
         public void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -58,7 +80,52 @@ namespace src
 
         public void btn_pow_Click(object sender, EventArgs e)
         {
+            if (sucet == 1)
+            {
+                string vymazat = Convert.ToString(a) + "+";
+                b = Convert.ToDouble(display.Text.Replace(vymazat, ""));
+                sucet = 0;
+                display.Text = Convert.ToString(math.Sucet(a, b));
+            }
+            else if (rozdiel == 1)
+            {
+                string vymazat = Convert.ToString(a) + "-";
+                b = Convert.ToDouble(display.Text.Replace(vymazat, ""));
+                rozdiel = 0;
+                display.Text = Convert.ToString(math.Rozdiel(a, b));
+            }
+            else if (nasobenie == 1)
+            {
+                string vymazat = Convert.ToString(a) + "*";
+                b = Convert.ToDouble(display.Text.Replace(vymazat, ""));
+                nasobenie = 0;
+                display.Text = Convert.ToString(math.Nasobenie(a, b));
+            }
+            else if (delenie == 1)
+            {
+                string vymazat = Convert.ToString(a) + "/";
+                b = Convert.ToDouble(display.Text.Replace(vymazat, ""));
+                delenie = 0;
+                display.Text = Convert.ToString(math.Delenie(a, b));
+            }
+            else if (mocnina == 1)
+            {
+                string vymazat = Convert.ToString(a) + "^";
+                b = Convert.ToDouble(display.Text.Replace(vymazat, ""));
+                mocnina = 0;
+                display.Text = Convert.ToString(math.Mocnina(a, b));
+            }
+            else if (faktorial == 1)
+            {
+                faktorial = 0;
+                display.Text = Convert.ToString(math.Faktorial(a));
+            }
 
+            a = Convert.ToDouble(display.Text);
+            mocnina = 1;
+            display.Text = display.Text + "^";
+            enter = 0;
+            bodka = 0;
         }
 
         public void Form1_Load(object sender, EventArgs e)
@@ -73,105 +140,189 @@ namespace src
 
         public void btn_8_Click(object sender, EventArgs e)
         {
-            if (display.Text == "0" || enter == 1)
+            if (faktorial == 1)
             {
-                display.Clear();
-                enter = 0;
+                display.Text = display.Text;
             }
-            
-            display.Text = display.Text + "8";
+            else
+            {
+                if (display.Text == "0" || enter == 1)
+                {
+                    display.Clear();
+                    enter = 0;
+                    bodka = 0;
+                }
+
+                display.Text = display.Text + "8";
+            }
         }
 
         public void btn_0_Click(object sender, EventArgs e)
         {
-            if (display.Text == "0" || enter == 1)
+            if (faktorial == 1)
             {
-                display.Clear();
-                enter = 0;
+                display.Text = display.Text;
             }
-           
-            display.Text = display.Text + "0";
+            else
+            {
+                if (display.Text == "0" || enter == 1)
+                {
+                    display.Clear();
+                    enter = 0;
+                    bodka = 0;
+                }
+
+                display.Text = display.Text + "0";
+            }
         }
 
         public void btn_dot_Click(object sender, EventArgs e)
         {
-           
-            
-            display.Text = display.Text + ",";
+            if (faktorial == 1)
+            {
+                display.Text = display.Text;
+            }   
+            else
+            { 
+                bodka = bodka + 1;
+                if (bodka > 1)
+                {
+                    display.Text = display.Text;
+                }
+                else
+                {
+                    display.Text = display.Text + ",";
+                }
+            }
         }
 
         public void btn_3_Click(object sender, EventArgs e)
         {
-            if (display.Text == "0" || enter == 1)
+            if (faktorial == 1)
             {
-                display.Clear();
-                enter = 0;
+                display.Text = display.Text;
             }
-            
-            display.Text = display.Text + "3";
+            else
+            {
+                if (display.Text == "0" || enter == 1)
+                {
+                    display.Clear();
+                    enter = 0;
+                    bodka = 0;
+                }
+
+                display.Text = display.Text + "3";
+            }
         }
 
         public void btn_2_Click(object sender, EventArgs e)
         {
-            if (display.Text == "0" || enter == 1)
+            if (faktorial == 1)
             {
-                display.Clear();
-                enter = 0;
+                display.Text = display.Text;
             }
-            
-            display.Text = display.Text + "2";
+            else
+            {
+                if (display.Text == "0" || enter == 1)
+                {
+                    display.Clear();
+                    enter = 0;
+                    bodka = 0;
+                }
+
+                display.Text = display.Text + "2";
+            }
         }
 
         public void btn_6_Click(object sender, EventArgs e)
         {
-            if (display.Text == "0" || enter == 1)
+            if (faktorial == 1)
             {
-                display.Clear();
-                enter = 0;
+                display.Text = display.Text;
             }
-            
-            display.Text = display.Text + "6";
+            else
+            {
+                if (display.Text == "0" || enter == 1)
+                {
+                    display.Clear();
+                    enter = 0;
+                    bodka = 0;
+                }
+
+                display.Text = display.Text + "6";
+            }
         }
 
         public void btn_5_Click(object sender, EventArgs e)
         {
-            if (display.Text == "0" || enter == 1)
+            if (faktorial == 1)
             {
-                display.Clear();
-                enter = 0;
+                display.Text = display.Text;
             }
-            
-            display.Text = display.Text + "5";
+            else
+            {
+                if (display.Text == "0" || enter == 1)
+                {
+                    display.Clear();
+                    enter = 0;
+                    bodka = 0;
+                }
+
+                display.Text = display.Text + "5";
+            }
         }
 
         public void btn_4_Click(object sender, EventArgs e)
         {
-            if (display.Text == "0" || enter == 1)
+            if (faktorial == 1)
             {
-                display.Clear();
-                enter = 0;
+                display.Text = display.Text;
             }
-            
-            display.Text = display.Text + "4";
+            else
+            {
+                if (display.Text == "0" || enter == 1)
+                {
+                    display.Clear();
+                    enter = 0;
+                    bodka = 0;
+                }
+
+                display.Text = display.Text + "4";
+            }
         }
 
         public void btn_9_Click(object sender, EventArgs e)
         {
-            if (display.Text == "0" || enter == 1)
+            if (faktorial == 1)
             {
-                display.Clear();
-                enter = 0;
+                display.Text = display.Text;
             }
-            
-            display.Text = display.Text + "9";
+            else
+            {
+                if (display.Text == "0" || enter == 1)
+                {
+                    display.Clear();
+                    enter = 0;
+                    bodka = 0;
+                }
+
+                display.Text = display.Text + "9";
+            }
         }
 
         public void btn_ac_Click(object sender, EventArgs e)
         {
             display.Clear();
             display.Text = "0";
+            sucet = 0;
+            nasobenie = 0;
+            delenie = 0;
+            rozdiel = 0;
             enter = 0;
-        }
+            bodka = 0;
+            mocnina = 0;
+            faktorial = 0;
+    }
 
         public void btn_add_Click(object sender, EventArgs e)
         {
@@ -203,11 +354,24 @@ namespace src
                 delenie = 0;
                 display.Text = Convert.ToString(math.Delenie(a, b));
             }
+            else if (mocnina == 1)
+            {
+                string vymazat = Convert.ToString(a) + "^";
+                b = Convert.ToDouble(display.Text.Replace(vymazat, ""));
+                mocnina = 0;
+                display.Text = Convert.ToString(math.Mocnina(a, b));
+            }
+            else if (faktorial == 1)
+            {
+                faktorial = 0;
+                display.Text = Convert.ToString(math.Faktorial(a));
+            }
 
             a = Convert.ToDouble(display.Text);
             sucet = 1;
             display.Text = display.Text + "+";
             enter = 0;
+            bodka = 0;
 
 
         }
@@ -242,11 +406,24 @@ namespace src
                 delenie = 0;
                 display.Text = Convert.ToString(math.Delenie(a, b));
             }
+            else if (mocnina == 1)
+            {
+                string vymazat = Convert.ToString(a) + "^";
+                b = Convert.ToDouble(display.Text.Replace(vymazat, ""));
+                mocnina = 0;
+                display.Text = Convert.ToString(math.Mocnina(a, b));
+            }
+            else if (faktorial == 1)
+            {
+                faktorial = 0;
+                display.Text = Convert.ToString(math.Faktorial(a));
+            }
 
             a = Convert.ToDouble(display.Text);
             rozdiel = 1;
             display.Text = display.Text + "-";
             enter = 0;
+            bodka = 0;
         }
 
         public void btn_div_Click(object sender, EventArgs e)
@@ -279,11 +456,24 @@ namespace src
                 delenie = 0;
                 display.Text = Convert.ToString(math.Delenie(a, b));
             }
+            else if (mocnina == 1)
+            {
+                string vymazat = Convert.ToString(a) + "^";
+                b = Convert.ToDouble(display.Text.Replace(vymazat, ""));
+                mocnina = 0;
+                display.Text = Convert.ToString(math.Mocnina(a, b));
+            }
+            else if (faktorial == 1)
+            {
+                faktorial = 0;
+                display.Text = Convert.ToString(math.Faktorial(a));
+            }
 
             a = Convert.ToDouble(display.Text);
             delenie = 1;
             display.Text = display.Text + "/";
             enter = 0;
+            bodka = 0;
         }
 
         public void btn_mul_Click(object sender, EventArgs e)
@@ -316,16 +506,72 @@ namespace src
                 delenie = 0;
                 display.Text = Convert.ToString(math.Delenie(a, b));
             }
+            else if (mocnina == 1)
+            {
+                string vymazat = Convert.ToString(a) + "^";
+                b = Convert.ToDouble(display.Text.Replace(vymazat, ""));
+                mocnina = 0;
+                display.Text = Convert.ToString(math.Mocnina(a, b));
+            }
+            else if (faktorial == 1)
+            {
+                faktorial = 0;
+                display.Text = Convert.ToString(math.Faktorial(a));
+            }
 
             a = Convert.ToDouble(display.Text);
             nasobenie = 1;
             display.Text = display.Text + "*";
             enter = 0;
+            
         }
 
         public void btn_fact_Click(object sender, EventArgs e)
         {
-
+            if (sucet == 1)
+            {
+                string vymazat = Convert.ToString(a) + "+";
+                b = Convert.ToDouble(display.Text.Replace(vymazat, ""));
+                sucet = 0;
+                display.Text = Convert.ToString(math.Sucet(a, b));
+            }
+            else if (rozdiel == 1)
+            {
+                string vymazat = Convert.ToString(a) + "-";
+                b = Convert.ToDouble(display.Text.Replace(vymazat, ""));
+                rozdiel = 0;
+                display.Text = Convert.ToString(math.Rozdiel(a, b));
+            }
+            else if (nasobenie == 1)
+            {
+                string vymazat = Convert.ToString(a) + "*";
+                b = Convert.ToDouble(display.Text.Replace(vymazat, ""));
+                nasobenie = 0;
+                display.Text = Convert.ToString(math.Nasobenie(a, b));
+            }
+            else if (delenie == 1)
+            {
+                string vymazat = Convert.ToString(a) + "/";
+                b = Convert.ToDouble(display.Text.Replace(vymazat, ""));
+                delenie = 0;
+                display.Text = Convert.ToString(math.Delenie(a, b));
+            }
+            else if (mocnina == 1)
+            {
+                string vymazat = Convert.ToString(a) + "^";
+                b = Convert.ToDouble(display.Text.Replace(vymazat, ""));
+                mocnina = 0;
+                display.Text = Convert.ToString(math.Mocnina(a, b));
+            }
+            else if (faktorial == 1)
+            {
+                faktorial = 0;
+                display.Text = Convert.ToString(math.Faktorial(a));
+            }
+            a = Convert.ToDouble(display.Text);
+            faktorial = 1;
+            display.Text = display.Text + "!";
+            enter = 0;
         }
 
         public void btn_sin_Click(object sender, EventArgs e)
@@ -363,9 +609,20 @@ namespace src
                 delenie = 0;
                 display.Text = Convert.ToString(math.Delenie(a, b));
             }
-
+            else if (mocnina == 1)
+            {
+                string vymazat = Convert.ToString(a) + "^";
+                b = Convert.ToDouble(display.Text.Replace(vymazat, ""));
+                mocnina = 0;
+                display.Text = Convert.ToString(math.Mocnina(a, b));
+            }
+            else if (faktorial == 1)
+            {
+                faktorial = 0;
+                display.Text = Convert.ToString(math.Faktorial(a));
+            }
             enter = 1;
-            
+
         }
 
         private void btn_sqrt_Click(object sender, EventArgs e)
