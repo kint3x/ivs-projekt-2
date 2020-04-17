f11926ea3dd9aeaca047b63f2db606abc5a8bcc9
xbenov00 <xbenov00@vutbr.cz>
2020-04-17 17:25:42 +0200

    TestPrubezneDoplneni

--- src/TDD_testy.cs
+++ src/TDD_testy.cs
@@ -23,6 +23,8 @@ namespace TDD
                 // sem pôjdu funkcie s testami postupne ako ich budeš robiť
                 SUM_TEST();
                 DIV_TEST();
+                SUB_TEST();
+                MUL_TEST();
 
                 file.WriteLine("</table></body></html>");
             }
@@ -46,8 +48,7 @@ namespace TDD
 
         // TESTY 
         void SUM_TEST()
-        {
-            
+        { 
             try
             {
                 // DO PODMIENKY VLOZIT FUNKCIU A OCAKAVANY VYSLEDOK, do Success_test("SEM IDE CISLO TESTU");
@@ -57,35 +58,71 @@ namespace TDD
                 if (math.Sucet(-5, 20) == 15) Success_test(2);
                 else Fail_test(string.Format("Expected: 15 Got: {0}", math.Sucet(-5, 20)), 2);
 
-                if (math.Sucet(-5, 20) == 10) Success_test(3);
-                else Fail_test(string.Format("Expected: 10 Got: {0}", math.Sucet(-5, 20)), 3);
+                if (math.Sucet(-10, 20) == 10) Success_test(3);
+                else Fail_test(string.Format("Expected: 10 Got: {0}", math.Sucet(-10, 20)), 3);
             }
             catch (InvalidOperationException exception)
             {
-                if (exception != null) // ak exception existuje nech j uvypise
+                if (exception != null) // ak exception existuje nech ju vypise
                 {
                     // exception bude mat test_ID -1 
                     Fail_test(string.Format("EXCEPTION = {0}",exception.Message), -1);
                 }
             }
-
-
         }
         void DIV_TEST()
         {
             try
             {
                 if (math.Delenie(10.5, 10) == 1.05) Success_test(4);
-                else Fail_test(string.Format("Expected: 1.5 Got: {0}", math.Delenie(10.5, 10)), 4);
+                else Fail_test(string.Format("Expected: 1.05 Got: {0}", math.Delenie(10.5, 10)), 4);
 
                 if (math.Delenie(10.5, 0) == 1.5) Success_test(5);
-                else Fail_test(string.Format("Expected: - Got: {0}", math.Delenie(10.5, 0)), 5);
+                else Fail_test(string.Format("Expected: 1.5 Got: {0}", math.Delenie(10.5, 0)), 5);
             }
             catch (InvalidOperationException exception)
             {
                 Fail_test(string.Format("EXCEPTION = {0}", exception.Message), -1);
             }
         }
+
+        void SUB_TEST()
+        {
+            try
+            {
+                if (math.Rozdiel(20, 8) == 12) Success_test(6);
+                else Fail_test(string.Format("Expected: 12 Got: {0}", math.Rozdiel(20, 8)), 6);
+
+                if (math.Rozdiel(0, -20) == -20) Success_test(7);
+                else Fail_test(string.Format("Expected: -20 Got: {0}", math.Rozdiel(20, 8)), 6);
+
+            }
+            catch (InvalidOperationException exception)
+            {
+                Fail_test(string.Format("EXCEPTION = {0}", exception.Message), -1);
+            }
+        }
+
+        void MUL_TEST()
+        {
+            try
+            {
+                if (math.Nasobenie(5, 7) == 35) Success_test(8);
+                else Fail_test(string.Format("Expected: 35 Got: {0}", math.Nasobenie(5, 7)), 8);
+
+                if (math.Nasobenie(-9, 6) == -54) Success_test(9);
+                else Fail_test(string.Format("Expected: -54 Got: {0}", math.Nasobenie(-9, 6)), 9);
+            }
+            catch (InvalidOperationException exception)
+            {
+                Fail_test(string.Format("EXCEPTION = {0}", exception.Message), -1);
+            }
+        }
+
+
+
+
+
 
         /**
          *  SABLONA AKO VYTVARAT FUNKCIE > POTOM KAZDU NOVU FUNKCIU (TEST) treba zaradit hore do vyznaceneho miesta vo funkcii Spusti_testy()
@@ -101,7 +138,7 @@ namespace TDD
          *  {
          *      Fail_test(string.Format("EXCEPTION = {0}", exception.Message), -1);
          *  }
-         */    
+         */
     }
 } 
   
