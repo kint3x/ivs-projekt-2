96a1febae2a8f1dae26323fa417e13a6c5d76a93
Martin Matějka <martin@matejka.xyz>
2020-04-20 12:50:54 +0200

    vymazanie docasnych testov

--- src/Program.cs
+++ src/Program.cs
@@ -20,13 +20,6 @@ namespace src
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
 
-            // DOCASNE TESTY
-            Console.WriteLine("SUCET 5+5: {0}\n", math.Sucet(5, 5));
-            Console.WriteLine("ABS -10.33: {0} \n ABS 15.58: {1}\n", math.Absolutna(-10.33), math.Absolutna(15.58));
-            Console.WriteLine("SIN 50: {0}\n", math.Sinus(50, 0.0000001));
-            Console.WriteLine("MOCNINA 2na3: {0}\n", math.Mocnina(2, 3));
-            Console.WriteLine("DRUHA ODMOCNINA Z 9: {0}\n", math.findSqrt(9));
-
             Console.WriteLine("TDD TESTY:");
             TDD_testy testy = new TDD_testy();
             testy.Spusti_testy();
